d83b9230f40b274d36802eafa2f7ce6c42f3be1a
Rafael <rafinha_fern@hotmail.com>
2023-09-06 12:05:07 -0300

    feat(comp-greeting): resized, responsive, and center align

--- src/app/_page/_components/greetingSection.tsx
+++ src/app/_page/_components/greetingSection.tsx
@@ -5,31 +5,34 @@ import { Icons } from '@/components/Icons'
 export const GreetingSection = () => {
   return (
     <>
-      <div className="ml-8 h-[200px] w-[200px]">
+      <div className="h-[175px] w-[175px] md:ml-8 md:h-[250px] md:w-[250px]">
         <Image
-          height={200}
-          width={200}
+          height={250}
+          width={250}
           alt=""
           src="/android-chrome-512x512.png"
         />
       </div>
-      <article className="items-center pt-4">
+      <article className="items-center pt-10 md:pt-4">
         <div className="w-full justify-center">
-          <h3 className="mr-9 flex text-2xl font-extrabold leading-tight tracking-tighter md:text-3xl">
+          <h3 className="mr-9 flex text-2xl font-extrabold leading-tight tracking-tighter md:text-4xl">
             Hello,
-            <Icons.HandMetal className="ml-4" scale={100} />
+            <Icons.HandMetal
+              className="ml-4 h-6 w-6 md:h-8 md:w-8"
+              size="auto"
+            />
           </h3>
-          <h2 className="text-xl font-thin leading-tight tracking-tighter md:text-2xl">
+          <h2 className="text-xl font-thin leading-tight tracking-tighter md:text-3xl">
             my name's <span className="font-bold">Rafael</span>
           </h2>
         </div>
-        <h2 className="block text-xl font-thin leading-tight tracking-tighter md:text-2xl">
+        <h2 className="block text-xl font-thin leading-tight tracking-tighter md:text-3xl">
           I like to <span className="font-bold">solve problems</span>
         </h2>
-        <h2 className="block text-xl font-thin leading-tight tracking-tighter md:text-2xl">
+        <h2 className="block text-xl font-thin leading-tight tracking-tighter md:text-3xl">
           And <span className="font-bold">build solutions</span>
         </h2>
-        <p className="block text-xl font-extrabold leading-tight tracking-tighter md:text-2xl">
+        <p className="block text-xl font-extrabold leading-tight tracking-tighter md:text-3xl">
           Code is just consequence
         </p>
       </article>
