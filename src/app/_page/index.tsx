38d50799401460a25d8c00b925c2999ead2aa037
Rafael <rafinha_fern@hotmail.com>
2023-09-13 19:46:10 -0300

    refactor(page): remove section tag and move it to components

--- src/app/_page/index.tsx
+++ src/app/_page/index.tsx
@@ -1,35 +1,22 @@
-import { ChessInvite } from './_components/chessInvite'
+import { ChessInviteSection } from './_components/chessInviteSection'
 import { FooterSection } from './_components/footerSection'
 import { GreetingSection } from './_components/greetingSection'
-import { LinksSection } from './_components/LinksSection'
+import { LinksSection } from './_components/linksSection'
 import { Marquee } from './_components/Marquee'
-import { MyworldxCard } from './_components/myworldxCard'
+import { MyworldxSection } from './_components/myworldxSection'
 import { PresentationSection } from './_components/presentationSection'
-import { Section } from './_components/section'
 
 export default function DefaultPage() {
   return (
     <>
       <main className="relative flex w-screen flex-col items-center gap-4 overflow-x-hidden">
-        <Section className="flex h-screen flex-wrap content-center items-center justify-center">
-          <GreetingSection />
-        </Section>
-        <Section className="flex items-end">
-          <PresentationSection />
-        </Section>
+        <GreetingSection />
+        <PresentationSection />
         <Marquee className="my-14" />
-        <Section>
-          <MyworldxCard />
-        </Section>
-        <Section>
-          <ChessInvite />
-        </Section>
-        <Section>
-          <LinksSection />
-        </Section>
-        <Section>
-          <FooterSection />
-        </Section>
+        <MyworldxSection />
+        <ChessInviteSection />
+        <LinksSection />
+        <FooterSection />
       </main>
     </>
   )
