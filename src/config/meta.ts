4cf33e334a8917f196bebe34e09c90f5bed69793
Rafael <rafinha_fern@hotmail.com>
2023-09-13 19:58:18 -0300

    feat(config): add default site metadata

---
+++ src/config/meta.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from '@/@types/meta'
+
+export const siteConfig: SiteConfig = {
+  name: 'Raferdev Software Engineer',
+  description: 'An open source landing page of Raferdev Software Engineer.',
+  url: 'https://rafer.dev',
+  ogImage: 'https://rafer.dev/og.jpg',
+  links: {
+    twitter: 'https://twitter.com/raferdev',
+    github: 'https://github.com/raferdev/landing-page',
+  },
+}
