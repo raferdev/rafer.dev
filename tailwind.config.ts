bfd7ae07729694889eff4106c49e44272702107e
Rafael <rafinha_fern@hotmail.com>
2023-09-13 19:49:16 -0300

    refactor(root): remove tailwind no used config
    - gitignore - new packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,6 @@ module.exports = {
         'scroll-left': 'scroll 10s linear infinite',
       },
       colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: {
@@ -21,7 +18,6 @@ module.exports = {
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
@@ -34,14 +30,6 @@ module.exports = {
         accent: {
           DEFAULT: 'hsl(var(--accent))',
           foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
         },
       },
       borderRadius: {
