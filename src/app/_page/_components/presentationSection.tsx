e1dd2ab863399ff187ab75589bbf1315e40a7960
Rafael <rafinha_fern@hotmail.com>
2023-09-13 19:38:39 -0300

    feat(components): add new presentation style
    - Change description - Add subtitle

--- src/app/_page/_components/presentationSection.tsx
+++ src/app/_page/_components/presentationSection.tsx
@@ -1,24 +1,35 @@
+import Balancer from 'react-wrap-balancer'
+
 import {
   PageHeader,
   PageHeaderDescription,
   PageHeaderHeading,
 } from '@/components/PageHeading'
+import { Section, SectionContent, SectionHeader } from '@/components/Section'
 
 import { WakatimeBadge } from './wakatimeBadge'
 
 export const PresentationSection = () => {
   return (
-    <>
+    <Section className="flex items-end">
       <PageHeader>
-        <PageHeaderHeading className="flex flex-wrap gap-4">
-          Software Engineer <WakatimeBadge className="w-[200px] md:w-[250px]" />
-        </PageHeaderHeading>
-        <PageHeaderDescription>
+        <SectionHeader>
+          <PageHeaderHeading className="flex flex-wrap items-baseline gap-4">
+            Software Engineer{' '}
+            <WakatimeBadge className="w-[180px] md:w-[200px]" />
+          </PageHeaderHeading>
+          <PageHeaderDescription>
+            Pragmatism with creativity to create new things
+          </PageHeaderDescription>
+        </SectionHeader>
+      </PageHeader>
+      <SectionContent className="ml-4">
+        <Balancer className="text-secondary-foreground text-base font-normal sm:text-lg">
           Working building web solutions with NodeJS, NextJS, tests with Jest,
           database SQL and NoSQL, Docker, and more. Deploy on server and
           serveless environments with CI/CD.
-        </PageHeaderDescription>
-      </PageHeader>
-    </>
+        </Balancer>
+      </SectionContent>
+    </Section>
   )
 }
