c566c553e5b394a40c8765e9e795dd450f9befc2
Rafael <rafinha_fern@hotmail.com>
2023-09-30 13:45:07 -0300

    feat(layout): add new metadata schema and file
    - Divided the metadata file from layout index

---
+++ src/app/_layout/metadata.tsx
@@ -0,0 +1,58 @@
+import { Metadata } from 'next'
+
+import { siteConfig } from '@/config/meta'
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+
+    template: `%s | ${siteConfig.name}`,
+  },
+
+  description: siteConfig.description,
+  referrer: 'origin-when-cross-origin',
+
+  keywords: ['Nextjs', 'Software Engineer', 'Open Source', 'Raferdev'],
+  authors: [
+    {
+      name: 'raferdev',
+      url: 'https://rafer.dev',
+    },
+  ],
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  creator: 'Rafael',
+  themeColor: [{ media: '(prefers-color-scheme: light)', color: 'white' }],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    images: [`${siteConfig.url}/og.jpg`],
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  alternates: {
+    canonical: siteConfig.url,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: '@raferdev',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+}
