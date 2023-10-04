7d8b9a34cd754d14dbb64612bc2a4f5f04e12ff9
Rafael <rafinha_fern@hotmail.com>
2023-10-04 07:16:57 -0300

    feat(page): add new grid styles and lazy loading on myworldx section

--- src/app/_page/_components/myworldxSection.tsx
+++ src/app/_page/_components/myworldxSection.tsx
@@ -13,7 +13,7 @@ import {
 
 export const MyworldxSection = () => {
   return (
-    <Section className="w-full border-0 bg-inherit shadow-none">
+    <Section className="min-h-64 w-full border-0 bg-inherit shadow-none">
       <SectionHeader className="space-y-1">
         <SectionTitle className="text-2xl">Check This Out</SectionTitle>
         <SectionDescription>
@@ -26,46 +26,46 @@ export const MyworldxSection = () => {
             loading="lazy"
             decoding="async"
             className="mr-4 h-20"
-            src="myworldx-logo.png"
-            alt="myworldx logo"
+            src="./logos/myworldx-logo.png"
+            alt=""
           />
           <Balance className="text-secondary-foreground text-base font-normal sm:text-lg">
             Myworldx it's builded to github user's can show your content more
             easily based on markdown files on your repo's.
           </Balance>
         </div>
-        <div className="grid grid-rows-3 place-content-center gap-2">
+        <div className="grid w-full grid-cols-1 grid-rows-3 place-content-center justify-items-center gap-y-4">
           <Button asChild>
             <a href="https://myworldx.dev">
               <p className="flex text-sm font-bold text-muted-foreground">
-                Myworldx -{' '}
-                <span className="flex text-center font-normal text-foreground">
-                  <Icons.home size={35} className="h-5" />
-                  Home
-                </span>
+                Myworldx
               </p>
+              <span className="ml-2 flex w-full text-center font-normal text-foreground">
+                <Icons.home size={16} className="mr-2 fill-icons-cyan-200" />
+                Home
+              </span>
             </a>
           </Button>
           <Button asChild>
             <a href="https://myworldx.dev/raferdev">
               <p className="flex text-sm font-bold text-muted-foreground ">
-                Myworldx -{' '}
-                <span className="flex text-center font-normal text-foreground ">
-                  <Icons.fileText size={35} className="h-5" />
-                  My page
-                </span>
+                Myworldx
               </p>
+              <span className="ml-2 flex w-full text-center font-normal text-foreground ">
+                <Icons.fileText size={16} className="mr-2 fill-icons-white" />
+                My page
+              </span>
             </a>
           </Button>
           <Button asChild>
             <a href="https://github.com/myworldx/myworldx">
               <p className="flex text-sm font-bold text-muted-foreground ">
-                Myworldx -{' '}
-                <span className="flex text-center font-normal text-foreground ">
-                  <Icons.github size={35} className="h-5" />
-                  Repo
-                </span>
+                Myworldx
               </p>
+              <span className="ml-2 flex w-full text-center font-normal text-foreground ">
+                <Icons.github size={16} className="mr-2 fill-icons-github" />
+                Repository
+              </span>
             </a>
           </Button>
         </div>
