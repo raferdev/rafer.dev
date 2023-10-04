f5d06864f2e57f34950e0ca549f3a47c843237e7
Rafael <rafinha_fern@hotmail.com>
2023-10-04 07:18:20 -0300

    feat(page): add new links and grid styles on links section
    - More instagram and twitter links - Grid cols 3,2 and 1

--- src/app/_page/_components/linksSection.tsx
+++ src/app/_page/_components/linksSection.tsx
@@ -10,43 +10,58 @@ import {
 
 export function LinksSection() {
   return (
-    <Section className="w-full border-0 bg-inherit shadow-none">
+    <Section className="min-h-64 w-full border-0 bg-inherit shadow-none">
       <SectionHeader className="space-y-1">
-        <SectionTitle className="text-2xl">See more</SectionTitle>
+        <SectionTitle id="links" className="text-2xl">
+          See more
+        </SectionTitle>
         <SectionDescription>
           You can see more about me in the following links
         </SectionDescription>
       </SectionHeader>
-      <SectionContent className="grid h-40 w-full place-content-center items-center gap-4">
-        <div className="grid grid-cols-2 gap-6">
-          <Button asChild>
-            <a href="https://github.com/raferdev" target="_blank">
-              <Icons.github className="mr-2 h-4 w-4 fill-black" />
-              Github
-            </a>
-          </Button>
-          <Button asChild>
-            <a href="https://youtube.com/raferdev" target="_blank">
-              <Icons.youtube className="mr-2 h-4 w-4 fill-red-400" />
-              Youtube
-            </a>
-          </Button>
-          <Button asChild>
-            <a href="https://linkedin.com/u/raferdev" target="_blank">
-              <Icons.linkedin className="mr-2 h-4 w-4 fill-sky-600" />
-              Linkedin
-            </a>
-          </Button>
-          <Button asChild>
-            <a
-              href="mailto:contact@rafer.dev?subject=Contact&body=Hello, i want talk about..."
-              target="_blank"
-            >
-              <Icons.email className="mr-2 h-4 w-4 fill-white" />
-              Email
-            </a>
-          </Button>
-        </div>
+      <SectionContent className="min-h-40 grid w-full grid-cols-1 place-content-center justify-items-center gap-y-4 md:grid-cols-2 lg:grid-cols-3">
+        <Button asChild>
+          <a href="https://github.com/raferdev" target="_blank">
+            <Icons.github className="mr-2 h-4 w-4 fill-icons-github" />
+            Github
+          </a>
+        </Button>
+        <Button asChild>
+          <a
+            href="https://www.youtube.com/channel/UClg1tYQi4CvnPRL2SAswVcg"
+            target="_blank"
+          >
+            <Icons.youtube className="mr-2 h-4 w-4 fill-icons-youtube" />
+            Youtube
+          </a>
+        </Button>
+        <Button asChild>
+          <a href="https://linkedin.com/u/raferdev" target="_blank">
+            <Icons.linkedin className="mr-2 h-4 w-4 fill-icons-linkedin" />
+            Linkedin
+          </a>
+        </Button>
+        <Button asChild>
+          <a
+            href="mailto:contact@rafer.dev?subject=Contact&body=Hello, i want talk about..."
+            target="_blank"
+          >
+            <Icons.email className="mr-2 h-4 w-4 fill-icons-white" />
+            Email
+          </a>
+        </Button>
+        <Button asChild>
+          <a href="https://twitter.com/raferdev" target="_blank">
+            <Icons.twitter className="mr-2 h-4 w-4 fill-icons-twitter" />
+            Twitter / X
+          </a>
+        </Button>
+        <Button asChild>
+          <a href="https://instagram.com/raferdev" target="_blank">
+            <Icons.instagram className="mr-2 h-4 w-4 fill-icons-instagram" />
+            Instagram
+          </a>
+        </Button>
       </SectionContent>
     </Section>
   )
