03b323b4499dc76b5a67312c107133ea48eaab2d
Rafael <rafinha_fern@hotmail.com>
2023-10-14 17:51:34 -0300

    fix(layout): fix RUM variables

--- src/app/_layout/rum.tsx
+++ src/app/_layout/rum.tsx
@@ -1,23 +1,24 @@
 import Script from 'next/script'
-import { version } from '@/../../package.json'
+
+import { __env } from '@/config/env'
+import { __site } from '@/config/site'
 
 export const RumAnalytics = () => {
   return (
     <Script id="rum-analytics">
-      {`
- (function(h,o,u,n,d) {
+      {`(function(h,o,u,n,d) {
    h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
    d=o.createElement(u);d.async=1;d.src=n
    n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
  })(window,document,'script','https://www.datadoghq-browser-agent.com/us1/v4/datadog-rum.js','DD_RUM')
  window.DD_RUM.onReady(function() {
    window.DD_RUM.init({
-     clientToken: ${process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN},
-     applicationId: ${process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID},
-     site: ${process.env.NEXT_PUBLIC_DATADOG_SITE},
+     clientToken: ${__env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN},
+     applicationId: ${__env.NEXT_PUBLIC_DATADOG_APPLICATION_ID},
+     site: ${__env.NEXT_PUBLIC_DATADOG_SITE},
      service: 'rafer.dev',
-     env: ${process.env.NEXT_PUBLIC_DATADOG_ENV},
-     version: ${version}, 
+     env: ${__env.NEXT_PUBLIC_DATADOG_ENV},
+     version: ${__site.rum.version}, 
      sessionSampleRate: 100,
      sessionReplaySampleRate: 20,
      trackUserInteractions: true,
@@ -27,8 +28,7 @@ export const RumAnalytics = () => {
    });
 
    window.DD_RUM.startSessionReplayRecording();
- })
-`}
+ })`}
     </Script>
   )
 }
