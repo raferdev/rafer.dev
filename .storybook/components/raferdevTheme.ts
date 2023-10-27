3119b656485495185a01ce49fb9a2e949dbf0a2c
Rafael <rafinha_fern@hotmail.com>
2023-10-27 18:42:13 -0300

    refactor(storybook): move root files to paths

--- .storybook/components/raferdevTheme.ts
+++ .storybook/components/raferdevTheme.ts
@@ -1,11 +1,13 @@
 import { create } from '@storybook/theming/create'
 
-import imageLogo from './public/logo-dark-theme.png'
+import imageLogo from '../public/logo-dark-theme.png'
 
 export default create({
   base: 'dark',
   brandTitle: 'Raferdev Storybook',
   brandUrl: 'https://storybook.rafer.dev',
-  brandImage: imageLogo,
+  brandImage: imageLogo.toString(),
   brandTarget: '_self',
+
+  fontBase: 'var(--font-sans)',
 })
