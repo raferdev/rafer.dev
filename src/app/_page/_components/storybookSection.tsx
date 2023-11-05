daabe6bfeec2d17f73140f0f0c344c4b0985d1bd
Rafael <raferdev@outlook.com>
2023-11-05 05:30:09 -0300

    feat(page): add new animation, updated text description and layout
    - New logo backlight animation

--- src/app/_page/_components/storybookSection.tsx
+++ src/app/_page/_components/storybookSection.tsx
@@ -1,48 +1,53 @@
 /* eslint-disable @next/next/no-img-element */
 
-import Balancer from 'react-wrap-balancer'
-
 import { __site } from '@/config/site'
 import { Link } from '@/components/Link'
 import { Section } from '@/components/Section'
 
-export const StorybookSection = () => {
-  const { links } = __site
-  return (
-    <Section.Container className="w-full border-0 bg-inherit shadow-none">
-      <Section.Header className="space-y-1">
-        <Section.Title className="text-2xl">Storybook</Section.Title>
-        <Section.Description>
-          You can view the docs on storybook
-        </Section.Description>
-      </Section.Header>
-      <Section.Content className="grid w-full grid-rows-2 gap-4 md:grid-cols-2 md:grid-rows-1">
-        <div className="flex items-center">
+const { links } = __site
+
+const { Container, Header, Title, Description, Content, SubSection } = Section
+
+const StorybookSection = () => (
+  <Container>
+    <Header>
+      <Title asChild>
+        <h3>Storybook</h3>
+      </Title>
+      <Description>You can view the documents on StoryBook</Description>
+    </Header>
+    <Content>
+      <SubSection.Content.Container>
+        <SubSection.Content.Illustration animationColor="rose-400">
           <img
             loading="lazy"
             decoding="async"
-            className="mr-6 h-20 w-20"
+            className="h-24"
             src="./logos/storybook.png"
             alt="storybook logo"
           />
+        </SubSection.Content.Illustration>
+        <SubSection.Content.Description>
+          <p>
+            I'm not a professional designer or UI/UX, but I love documentation,
+            and Storybook nailed it. You can see more about the components,
+            logos, and even system architecture of this website here.
+          </p>
+        </SubSection.Content.Description>
+      </SubSection.Content.Container>
+      <SubSection.Links.Container>
+        <Link
+          iconFillColor="storybook"
+          iconName="BookOpen"
+          target="_blank"
+          className="md:row-start-2"
+          href={links.storybook}
+        >
+          About
+        </Link>
+      </SubSection.Links.Container>
+    </Content>
+  </Container>
+)
 
-          <Balancer className="text-secondary-foreground text-base font-normal sm:text-lg">
-            I'm not a designer, UI/UX, but i love documentation and storybook.
-            You can see more about components, logo, and more here.
-          </Balancer>
-        </div>
-        <div className="grid w-full grid-cols-1 place-content-center justify-items-center gap-2">
-          <Link
-            iconFillColor="storybook"
-            iconName="BookOpen"
-            label="Storybook"
-            target="_blank"
-            href={links.storybook}
-          >
-            Docs
-          </Link>
-        </div>
-      </Section.Content>
-    </Section.Container>
-  )
-}
+export { StorybookSection }
