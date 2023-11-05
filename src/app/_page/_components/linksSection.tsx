56796a5666711266f1eaacb44bf4193cbc7cfccc
Rafael <raferdev@outlook.com>
2023-11-05 05:24:00 -0300

    feat(page): add new link section layout dimensions

--- src/app/_page/_components/linksSection.tsx
+++ src/app/_page/_components/linksSection.tsx
@@ -2,68 +2,71 @@ import { __site } from '@/config/site'
 import { Link } from '@/components/Link'
 import { Section } from '@/components/Section'
 
-export const LinksSection = () => {
-  const { links } = __site
-  return (
-    <Section.Container className="w-full border-0 bg-inherit shadow-none">
-      <Section.Header className="space-y-1">
-        <Section.Title id="links" className="text-2xl">
-          See more
-        </Section.Title>
-        <Section.Description>
-          You can see more about me in the following links
-        </Section.Description>
-      </Section.Header>
-      <Section.Content className="min-h-40 grid w-full grid-cols-1 place-content-center justify-items-center gap-y-4 p-0 md:grid-cols-2 lg:grid-cols-3">
-        <Link
-          iconFillColor="github"
-          iconName="Github"
-          href={links.profile.github}
-          target="_blank"
-        >
-          Github
-        </Link>
-        <Link
-          iconFillColor="youtube"
-          iconName="Youtube"
-          href={links.profile.youtube}
-          target="_blank"
-        >
-          Youtube
-        </Link>
-        <Link
-          iconFillColor="linkedin"
-          iconName="Linkedin"
-          href={links.profile.linkedin}
-          target="_blank"
-        >
-          Linkedin
-        </Link>
-        <Link
-          iconFillColor="white"
-          iconName="Mail"
-          href={links.profile.email.raferdev}
-          target="_blank"
-        >
-          Email
-        </Link>
-        <Link
-          iconFillColor="twitter"
-          iconName="Twitter"
-          href={links.profile.twitter}
-          target="_blank"
-        >
-          Twitter / X
-        </Link>
-        <Link
-          iconFillColor="instagram"
-          iconName="Instagram"
-          href={links.profile.instagram}
-          target="_blank"
-        >
-          Instagram
-        </Link>
-      </Section.Content>
-    </Section.Container>
-  )
-}
+const { links } = __site
+
+const { Container, Header, Title, Description, Content } = Section
+
+const LinksSection = () => (
+  <Container>
+    <Header className="space-y-1">
+      <Title className="text-2xl" asChild>
+        <h5>See more</h5>
+      </Title>
+      <Description>
+        You can see more about me at the following links:
+      </Description>
+    </Header>
+    <Content className="min-h-40 grid w-full grid-cols-1 justify-items-center gap-x-8 gap-y-4 p-8 md:grid-cols-2 md:gap-y-4 lg:grid-cols-3 lg:grid-rows-2 lg:gap-y-0">
+      <Link
+        iconFillColor="github"
+        iconName="Github"
+        href={links.profile.github}
+        target="_blank"
+      >
+        Github
+      </Link>
+      <Link
+        iconFillColor="youtube"
+        iconName="Youtube"
+        href={links.profile.youtube}
+        target="_blank"
+      >
+        Youtube
+      </Link>
+      <Link
+        iconFillColor="linkedin"
+        iconName="Linkedin"
+        href={links.profile.linkedin}
+        target="_blank"
+      >
+        Linkedin
+      </Link>
+      <Link
+        iconFillColor="white"
+        iconName="Mail"
+        href={links.profile.email.raferdev}
+        target="_blank"
+      >
+        Email
+      </Link>
+      <Link
+        iconFillColor="twitter"
+        iconName="Twitter"
+        href={links.profile.twitter}
+        target="_blank"
+      >
+        Twitter / X
+      </Link>
+      <Link
+        iconFillColor="instagram"
+        iconName="Instagram"
+        href={links.profile.instagram}
+        target="_blank"
+      >
+        Instagram
+      </Link>
+    </Content>
+  </Container>
+)
+
+export { LinksSection }
