7c10e42630028ef2d046d466ef5a7e339c542cdc
Rafael <raferdev@outlook.com>
2023-11-06 16:49:07 -0300

    feat(pages): changes on logo page layout and image opacity

--- src/stories/content/about/pages/Logo/illustrationImg.tsx
+++ src/stories/content/about/pages/Logo/illustrationImg.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
-import LogoDraft from '@/stories/assets/logo-drafts.png'
+import LogoDraft from '@/stories/assets/logo-drafts.jpg'
 import { cn } from '@/utils/cn'
 
 const IllustrationImg = ({ className }: { className: string }) => {
   return (
     <div
       className={cn(
-        'm-0 flex max-h-80 w-full items-center justify-center border-0 align-middle md:overflow-hidden',
+        '-z-10 m-0 flex max-h-80 w-full items-center justify-center border-0 align-middle opacity-60 md:opacity-60',
         className
       )}
     >
