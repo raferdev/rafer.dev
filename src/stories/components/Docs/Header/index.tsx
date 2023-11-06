f7184af9b7e90bfc31ba58f6d330c84295fcb588
Rafael <raferdev@outlook.com>
2023-11-06 16:42:51 -0300

    feat(stories): rewrite docs component, refactored and layout changes

---
+++ src/stories/components/Docs/Header/index.tsx
@@ -0,0 +1,28 @@
+import { ContainerProps } from '@/@types/stories'
+import { cn } from '@/utils/cn'
+import { Slot } from '@radix-ui/react-slot'
+
+import { SubTitle } from './subtitle'
+import { Title } from './title'
+
+const Container = ({ className, asChild, ...props }: ContainerProps) => {
+  const Comp = asChild ? Slot : 'div'
+
+  return (
+    <Comp
+      className={cn(
+        'box-border grid h-[12rem] w-full grid-cols-1 md:grid-cols-2',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+const Header = {
+  Container,
+  SubTitle,
+  Title,
+}
+
+export { Header }
