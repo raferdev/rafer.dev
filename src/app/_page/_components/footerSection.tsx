0ee5203926883fa1eb14b922749af0174e65e6d1
Rafael <raferdev@outlook.com>
2023-11-09 23:28:15 -0300

    fix(page): fix breaking display on medium size
    - Changed grid to flex layout

--- src/app/_page/_components/footerSection.tsx
+++ src/app/_page/_components/footerSection.tsx
@@ -12,22 +12,27 @@ const FooterSection = () => (
     asChild
   >
     <footer>
-      <Section.Content className="flex flex-col py-0 md:items-end md:justify-end md:gap-0 lg:grid lg:grid-cols-1 lg:items-end lg:justify-end">
+      <Section.Content className="flex flex-col py-0 md:items-end md:justify-end md:gap-0 lg:flex lg:flex-col lg:items-end lg:justify-end">
         <Balancer className="max-h-50px w-full text-center text-slate-500 drop-shadow-md lg:text-start">
-          This landing page is open source, and you can find it
+          This landing page is open source, and you can find it here,
           <Link
             href={links.self.repository}
             target="_blank"
             variant="raw"
-            size="sm"
-            className="focus-visible: m-0 p-0"
+            aria-label="Github Repository"
+            className="focus-visible: m-0 w-0 min-w-min p-0 text-muted-foreground md:min-w-min"
           >
-            <span className="text-base sm:text-lg">here</span>
+            <span
+              className="text-base sm:text-lg"
+              aria-label="github repository"
+            >
+              github.com
+            </span>
           </Link>
           . See you soon!
         </Balancer>
       </Section.Content>
-      <Section.Footer className="flex w-full items-center justify-center text-sm text-muted-foreground">
+      <Section.Footer className="flex w-full items-center justify-center text-sm text-slate-500">
         raferdev@2023
       </Section.Footer>
     </footer>
