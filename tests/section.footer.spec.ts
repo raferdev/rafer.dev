7f07a0b40e9bb3acadf46fceaa4072eb711b2d21
Rafael <raferdev@outlook.com>
2023-11-14 00:51:17 -0300

    test(playwright): add new footer basic tests
    - Texts and links

---
+++ tests/section.footer.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test'
+
+const HOME = '/'
+
+test.describe('Section - Footer', () => {
+  const FOOTER_TEXT =
+    'This landing page is open source, and you can find it here'
+  const FOOTER_CORPORATE = 'raferdev@2023'
+
+  test('Expected text to be visible', async ({ page }) => {
+    await page.goto(HOME)
+
+    const PAGE_LOCATOR = page.locator('footer')
+    const textFooter = PAGE_LOCATOR.getByText(FOOTER_TEXT)
+
+    await expect(textFooter).toBeVisible()
+  })
+  test('Expected corporate text to be visible', async ({ page }) => {
+    await page.goto(HOME)
+
+    const PAGE_LOCATOR = page.locator('footer')
+    const textFooter = PAGE_LOCATOR.getByText(FOOTER_CORPORATE)
+
+    await expect(textFooter).toBeVisible()
+  })
+})
