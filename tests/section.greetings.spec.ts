62e45d94266166542dd9904dcfa1e974f2b38519
Rafael <raferdev@outlook.com>
2023-11-14 00:52:42 -0300

    test(playwright): add new greetings basic tests
    - Image and texts

---
+++ tests/section.greetings.spec.ts
@@ -0,0 +1,57 @@
+import { expect, test } from '@playwright/test'
+
+const HOME = '/'
+
+test.describe('Section - Greetings', () => {
+  const SECTION_POSITION = 0
+  const LOGO_SRC = 'logo-250x250.png'
+  const SECTION_P_ONE = " Hi! My name's Rafael"
+  const SECTION_P_TWO = 'I like to solve problems'
+  const SECTION_P_THREE = 'And build solutions'
+  const SECTION_P_FOUR = 'Code is just a consequence'
+
+  test('Expected - Logo image to be visible', async ({ page }) => {
+    await page.goto(HOME)
+
+    const PAGE_LOCATOR = page.locator('section').nth(SECTION_POSITION)
+
+    const logo = PAGE_LOCATOR.locator('img')
+    const logoName = await PAGE_LOCATOR.locator('img').getAttribute('src')
+
+    expect(logoName).toContain(LOGO_SRC)
+    await expect(logo).toBeVisible()
+  })
+
+  test('Expected - Greetings text to be visible', async ({ page }) => {
+    await page.goto(HOME)
+
+    const PAGE_LOCATOR = page.locator('section').nth(SECTION_POSITION)
+
+    const pOne = PAGE_LOCATOR.getByText(SECTION_P_ONE)
+    const pTwo = PAGE_LOCATOR.getByText(SECTION_P_TWO)
+    const pThree = PAGE_LOCATOR.getByText(SECTION_P_THREE)
+    const pFour = PAGE_LOCATOR.getByText(SECTION_P_FOUR)
+
+    expect(pOne).toBeVisible()
+    expect(pTwo).toBeVisible()
+    expect(pThree).toBeVisible()
+    expect(pFour).toBeVisible()
+  })
+  test('Expected - Correct height and width, full viewport', async ({
+    page,
+  }) => {
+    await page.goto(HOME)
+
+    const PAGE_LOCATOR = page.locator('section').nth(SECTION_POSITION)
+
+    const firstSectiongBox = await PAGE_LOCATOR.boundingBox()
+    const viewPort = page.viewportSize()
+
+    if (viewPort === null) throw new Error('Viewport is null')
+
+    const { width, height } = viewPort
+
+    expect(firstSectiongBox?.height).toBe(height)
+    expect(firstSectiongBox?.width).toBe(width)
+  })
+})
