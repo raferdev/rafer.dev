86c3c3b16a58078860c7fd25ec1ed55e47d1cbaa
Rafael <raferdev@outlook.com>
2023-11-29 11:52:52 -0300

    feat(layout): change layout background

--- src/app/_layout/index.tsx
+++ src/app/_layout/index.tsx
@@ -19,7 +19,7 @@ const DefaultLayout = ({ children }: Children) => (
       <RumAnalytics />
       <body
         className={cn(
-          'flex min-h-screen items-center justify-center bg-background bg-gradient-to-b from-gray-900 to-gray-600 font-sans antialiased',
+          'flex min-h-screen items-center justify-center bg-background bg-gradient-to-b from-gray-950 from-50% via-gray-800 to-sky-500 font-sans antialiased',
           fontSans.variable
         )}
       >
